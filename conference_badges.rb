a3711389be33ee113a83195e576923c9dfaeafad
Kevin C Malin <racmalin@yahoo.com>
2020-04-22 03:15:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is Arel."
+ 
+end
+def batch_badge_creator(attendees)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+   attendees.each = badges
+   puts "Hello, my name is #{attendees}."
+end
