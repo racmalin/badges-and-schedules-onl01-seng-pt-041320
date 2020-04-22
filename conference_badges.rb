d8438925b64865bce0c9dfca6520623902a35d14
Kevin C Malin <racmalin@yahoo.com>
2020-04-22 03:24:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,6 @@ def badge_maker(name)
 end
 def batch_badge_creator(attendees)
   attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-   
+   attendees.each do |number|
+
 end
